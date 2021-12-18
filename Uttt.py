5e127990323a20a5992031eba0fd561a17ee26ea
João Guilherme <alvesjg16@gmail.com>
2021-12-18 18:42:41 -0300

    ajustando Uttt.py para ser o arquivo que realmente roda o jogo

--- Uttt.py
+++ Uttt.py
@@ -2,9 +2,13 @@ from boards import *
 from players import *
 
 def main():
-    player1 = Noob("X")
-    player2 = Human("O")
-    game = Game(player1, player2)
+    players = []
+    print("Choosing the players type:")
+    for i in range(2):
+        type = int(input("Type 1 for Human,2 for Noob or any another number for Clumsy:"))
+        mark = "X" if i%2 == 0 else "O"
+        players.append( Human(mark) if type == 1 else(Noob(mark) if type==2 else (Clumsy(mark))) )
+    game = Game(*players)
     game.playing()
 
 if __name__ == "__main__":
