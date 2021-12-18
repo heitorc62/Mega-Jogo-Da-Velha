cad04ec516fcff79d0edd2a6f6a990dc7e385dff
João Guilherme <alvesjg16@gmail.com>
2021-12-18 18:52:15 -0300

    erro ortográfico

--- Uttt.py
+++ Uttt.py
@@ -5,7 +5,7 @@ def main():
     players = []
     print("Choosing the players type:")
     for i in range(2):
-        type = int(input("Type 1 for Human,2 for Noob or any another number for Clumsy:"))
+        type = int(input("Type 1 for Human,2 for Noob or another number for Clumsy:"))
         mark = "X" if i%2 == 0 else "O"
         players.append( Human(mark) if type == 1 else(Noob(mark) if type==2 else (Clumsy(mark))) )
     game = Game(*players)
