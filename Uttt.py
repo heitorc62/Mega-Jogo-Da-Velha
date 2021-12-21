76b589c66af5ada7368f4a747ebf50f4d032851c
João Guilherme <alvesjg16@gmail.com>
2021-12-21 16:36:29 -0300

    adicionando qual o jogador que vai responder a pergunta no tipo

--- Uttt.py
+++ Uttt.py
@@ -5,8 +5,8 @@ def main():
     players = []
     print("Choosing the players type:")
     for i in range(2):
-        type = int(input("Type 1 for Human,2 for Noob or another number for Clumsy:"))
         mark = "X" if i%2 == 0 else "O"
+        type = int(input(f"Player {mark}: Type 1 for Human,2 for Noob or another number for Clumsy:")) 
         players.append( Human(mark) if type == 1 else(Noob(mark) if type==2 else (Clumsy(mark))) )
     game = Game(*players)
     game.playing()
